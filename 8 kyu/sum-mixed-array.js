eb53aa0d0462b037db54d865eadb12d114b67232
KirillHubarevich <kgubarevich@inbox.ru>
2025-10-09 22:58:56 +0300

    sum-mixed-array

--- 8 kyu/sum-mixed-array.js
+++ 8 kyu/sum-mixed-array.js
@@ -5,15 +5,6 @@
 //
 //     Верните свой ответ в виде числа.
 
-// function sumMix(arr) {
-//     let sum = 0
-//     for (let i = 0; i < arr.length; i++) {
-//         arr = arr.map(Number)
-//         sum += arr[i]
-//     } return sum
-// }
-// let x = sumMix([1,2,3,'4','5','7'])
-// console.log(x)
 
 function sumMix(x) {
     let sum = 0
@@ -22,6 +13,6 @@ function sumMix(x) {
         sum += x[i]
     } return sum
 }
-let y = sumMix([1,2,3,'4','5','7'])
-console.log(y)
+
+
 
