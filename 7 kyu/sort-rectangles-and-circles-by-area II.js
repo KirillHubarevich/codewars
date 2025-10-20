56ce123d05a3d28e1a6dbc0acb707e9ac0bef0ae
KirillHubarevich <kgubarevich@inbox.ru>
2025-10-20 21:29:03 +0300

    sort-rectangles-and-circles-by-area II

---
+++ 7 kyu/sort-rectangles-and-circles-by-area II.js
@@ -0,0 +1,18 @@
+// In this kata you will be given a sequence of the dimensions of rectangles ( sequence with width and length ) and circles ( radius - just a number ).
+// Your task is to return a new sequence of dimensions, sorted ascending by area.
+
+
+//     const array = [ [4.23, 6.43], 1.23, 3.444, [1.342, 3.212] ]; // [ rectangle, circle, circle, rectangle ]
+// sortByArea(array) => [ [ 1.342, 3.212 ], 1.23, [ 4.23, 6.43 ], 3.444 ]
+
+
+//     function sortByArea(array) {
+//     return array;
+// }
+
+
+const area = x => Array.isArray(x) ? x[0]*x[1] : Math.PI*x*x;
+const sortByArea = array => array.slice().sort((a,b) => area(a) - area(b))
+const x = sortByArea([ [4.23, 6.43], 1.23, 3.444, [1.342, 3.212] ])
+console.log(x)
+
